748b019484c7f5a695428968f1d3a6d5e7b3c2bf
nounumm <nou.numm@gmail.com>
2026-03-26 22:52:00 +0900

    Initial Commit

---
+++ 집에가자/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  집에가자
+//
+//  Created by 김지윤 on 3/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
